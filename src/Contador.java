6a63c171d536706ca1adf3fada02114acb3464c5
SylviuzzzT <sthomaz56@gmail.com>
2024-07-17 11:23:20 -0300

    PROJETO DESAFIO JAVA

---
+++ src/Contador.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class Contador {
+     public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+
+        System.out.println("Informe o primeiro parâmetro: ");
+        int parametroUm = terminal.nextInt();
+
+        System.out.println("Informe o segundo parâmetro: ");
+        int parametroDois = terminal.nextInt();
+
+        try {
+            contar(parametroUm, parametroDois);
+        }
+        catch (ParametrosInvalidosException exception){
+            System.out.println(exception.getMessage());
+        }
+    }
+
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException{
+        if(parametroUm > parametroDois){
+            throw new ParametrosInvalidosException("O segundo parametro deve ser maior que o primeiro");
+        }
+
+        int contagem = parametroDois - parametroUm;
+        for(int i = 1; i <= contagem; i++){
+            System.out.println("Imprimindo numero: "+i);
+
+        }
+    }
+
+
+
+}
